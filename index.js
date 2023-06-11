40edf1d40418ee22d212bc414474f2603979ecad
Yang <weiyang.tan@weikegaming.com>
2023-06-11 09:08:42 +0800

    Test checkout files

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try { 
-  const nameToGreet = core.getInput('files-to-check');
-  console.log(`Hello ${nameToGreet}!`); 
-  
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const changes = core.getInput('github-output');
+    console.log(`Changes ${changes}!`); 
+
+    const auditFiles = core.getInput('files-to-check');
+    console.log(`Hello ${auditFiles}!`); 
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
