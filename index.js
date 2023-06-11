21fb696c549f9a1806e16590c213cb69232df135
Yang <weiyang.tan@weikegaming.com>
2023-06-11 09:41:18 +0800

    Test actual stuff

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+function findSimilarValues(array1, array2) {
+    // Create a set from array1
+    const set = new Set(array1);
+  
+    // Find all common values in array2
+    const similarValues = array2.filter(value => set.has(value));
+  
+    return similarValues;
+}
+
 try { 
     const changesInput = core.getInput('github-output');
     console.log(`Changes : ${changesInput}`); 
@@ -12,8 +22,14 @@ try {
         console.log(`File Names : ${filenames}`); 
     }
 
-    const auditFiles = core.getInput('files-to-check');
-    console.log(`Hello ${auditFiles}!`); 
+    const auditFilesInput = core.getInput('files-to-check');
+    const auditFiles = auditFilesInput.split(" ");
+
+    const similar = findSimilarValues(filenames, auditFiles);
+    if(similar.length > 0) {
+        const combined = similar.join(' ');
+        core.setFailed(combined);
+    }
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
